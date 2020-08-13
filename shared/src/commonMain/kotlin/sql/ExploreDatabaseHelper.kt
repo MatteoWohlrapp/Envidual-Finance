9b6a6d6c9d62b6caf15e7834cd729e3f7c16fa70
matteowohlrapp <matteo.wohlrapp@tum.de>
2020-08-13 18:20:18 +0200

    changed database helper to fit the database needs

--- shared/src/commonMain/kotlin/sql/ExploreDatabaseHelper.kt
+++ shared/src/commonMain/kotlin/sql/ExploreDatabaseHelper.kt
@@ -1,8 +1,7 @@
 package sql
 
 import co.example.envidual.finance.touchlab.db.EnvidualFinanceDatabase
-import co.example.envidual.finance.touchlab.db.Stockdata
-import co.touchlab.kermit.Kermit
+import co.example.envidual.finance.touchlab.db.Explorecompanydata
 import com.squareup.sqldelight.db.SqlDriver
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -12,41 +11,50 @@ class ExploreDatabaseHelper(
     sqlDriver: SqlDriver,
     private val backgroundDispatcher: CoroutineDispatcher
 ) {
-    private val dbRef: EnvidualFinanceDatabase = EnvidualFinanceDatabase(sqlDriver)
+    private val dbReference = EnvidualFinanceDatabase(sqlDriver)
 
 
-    fun selectAllItems(): Flow<List<Stockdata>> =
-        dbRef.tableQueries
+    fun selectAllItems(): Flow<List<Explorecompanydata>> =
+        dbReference.tableQueries
             .selectAll()
             .asFlow()
             .mapToList()
             .flowOn(backgroundDispatcher)
 
-    suspend fun insertStockData(tickers: List<String>) {
-        dbRef.transactionWithContext(backgroundDispatcher) {
-            tickers.forEach { ticker ->
-                dbRef.tableQueries.insertStock(null, ticker)
+    suspend fun insertExploreCompanyData(explorecompanydata: List<Explorecompanydata>) {
+        dbReference.transactionWithContext(backgroundDispatcher) {
+            explorecompanydata.forEach { explorecompanydata ->
+                dbReference.tableQueries.insertStock(null, explorecompanydata.country, explorecompanydata.finnhubIndustry, explorecompanydata.ipo,
+                explorecompanydata.logo, explorecompanydata.marketCapitalization, explorecompanydata.name, explorecompanydata.ticker)
             }
         }
     }
 
-    suspend fun selectById(id: Long): Flow<List<Stockdata>> =
-        dbRef.tableQueries
+    suspend fun selectById(id: Long): Flow<List<Explorecompanydata>> =
+        dbReference.tableQueries
             .selectById(id)
             .asFlow()
             .mapToList()
             .flowOn(backgroundDispatcher)
 
-    suspend fun selectByName(name: String): Flow<List<Stockdata>> =
-        dbRef.tableQueries
-            .selectByName(name)
+    suspend fun selectByTicker(name: String): Flow<List<Explorecompanydata>> =
+        dbReference.tableQueries
+            .selectByTicker(name)
             .asFlow()
             .mapToList()
             .flowOn(backgroundDispatcher)
 
+    suspend fun selectByCategory(name: String): Flow<List<Explorecompanydata>> =
+        dbReference.tableQueries
+            .selectByIndustry(name)
+            .asFlow()
+            .mapToList()
+            .flowOn(backgroundDispatcher)
+
+
     suspend fun deleteAll() {
-        dbRef.transactionWithContext(backgroundDispatcher) {
-            dbRef.tableQueries.deleteAll()
+        dbReference.transactionWithContext(backgroundDispatcher) {
+            dbReference.tableQueries.deleteAll()
         }
     }
 }
