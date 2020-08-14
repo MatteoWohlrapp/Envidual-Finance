e103cc5ce4449f5735ea86c6944d2c9ee45f7f8a
matteowohlrapp <matteo.wohlrapp@tum.de>
2020-08-14 10:16:53 +0200

    local changes to getCompanyForExploreUseCase

--- shared/src/commonMain/kotlin/domain.use_cases/GetCompanysForExploreUseCase.kt
+++ shared/src/commonMain/kotlin/domain.use_cases/GetCompanysForExploreUseCase.kt
@@ -1,18 +1,23 @@
 package domain.use_cases
 
 import domain.data.CompanyData
+import kotlinx.coroutines.flow.collect
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import remote.RemoteFinanceInterface
+import sql.ExploreDatabaseHelper
 
 class GetCompanysForExploreUseCase : KoinComponent {
 
     private val remoteFinance: RemoteFinanceInterface by inject()
+    private val exploreDatabaseHelper: ExploreDatabaseHelper by inject()
+
     private val exploreCompaniesTicker =
         mutableListOf("MSFT", "AAPL", "AMZN", "FB", "GOOGL", "IBM", "BRK.B")
 
 
     suspend fun invoke(): List<CompanyData> {
+        val data = exploreDatabaseHelper.selectAllItems().collect {  }
         val exploreList = mutableListOf<CompanyData>()
         for (ticker in exploreCompaniesTicker) {
             val data = remoteFinance.getCompanyData(ticker)
