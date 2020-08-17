3e77c11dbae81448bd912c0b486086810b25763a
maximilian.fehrentz@web.de <maximilian.fehrentz@web.de>
2020-08-17 12:02:00 +0200

    added the new use cases to the NativeViewModel

--- shared/src/iosMain/kotlin/NativeViewModel.kt
+++ shared/src/iosMain/kotlin/NativeViewModel.kt
@@ -2,9 +2,9 @@ package co.touchlab.kampkit
 
 import co.touchlab.stately.ensureNeverFrozen
 import domain.data.CompanyData
-import domain.use_cases.GetCompaniesForFavouritesUseCase
-import domain.use_cases.GetCompanyByTickerUseCase
+import domain.use_cases.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 
@@ -17,11 +17,18 @@ class NativeViewModel(
     private val scope = MainScope(Dispatchers.Main)
     private var getCompanyByTickerUseCase: GetCompanyByTickerUseCase
     private var getCompaniesForFavouritesUseCase: GetCompaniesForFavouritesUseCase
+    private var getCompaniesForSearchesUseCase: GetCompaniesForSearchesUseCase
+    private var addCompanyToFavouritesUseCase : AddCompanyToFavouritesUseCase
+    private var removeCompanyFromFavouritesUseCase : RemoveCompanyFromFavouritesUseCase
+
 
     init {
         ensureNeverFrozen()
         getCompanyByTickerUseCase = GetCompanyByTickerUseCase()
         getCompaniesForFavouritesUseCase = GetCompaniesForFavouritesUseCase()
+        getCompaniesForSearchesUseCase = GetCompaniesForSearchesUseCase()
+        addCompanyToFavouritesUseCase = AddCompanyToFavouritesUseCase()
+        removeCompanyFromFavouritesUseCase = RemoveCompanyFromFavouritesUseCase()
     }
 
     fun getCompanyByTicker(ticker:String) {
@@ -34,9 +41,34 @@ class NativeViewModel(
     fun getCompaniesForExplore() {
         scope.launch {
             val companyData = getCompaniesForFavouritesUseCase.invoke()
-            viewUpdate(companyData)
+            companyData.collect {
+                viewUpdate(it)
+            }
         }
     }
+
+    fun startObservingSearches() {
+        scope.launch {
+            val companyData = getCompaniesForSearchesUseCase.invoke()
+            companyData.collect {
+                viewUpdate(it)
+            }
+        }
+    }
+
+    fun addFavorite(company: CompanyData) {
+        scope.launch {
+            addCompanyToFavouritesUseCase.invoke(company)
+        }
+    }
+
+    fun removeFavorite(company: CompanyData) {
+        scope.launch {
+            removeCompanyFromFavouritesUseCase.invoke(company)
+        }
+    }
+
+
 
     fun onDestroy() {
         scope.onDestroy()
