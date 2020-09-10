0147d94523b62582cf8c6f87be9bf3ac9d9b965f
matteowohlrapp <matteo.wohlrapp@tum.de>
2020-09-10 16:55:48 +0200

    removed import.

--- shared/src/commonMain/kotlin/remote/RemoteFinance.kt
+++ shared/src/commonMain/kotlin/remote/RemoteFinance.kt
@@ -1,35 +1,13 @@
 package remote
 
-import cache.getThread
-import co.touchlab.kampkit.ktor.network
-import co.touchlab.stately.ensureNeverFrozen
 import co.touchlab.stately.isFrozen
-import com.squareup.sqldelight.internal.Atomic
-import com.squareup.sqldelight.internal.getValue
 import domain.data.CompanyData
 import domain.data.CompanyNews
 import domain.use_cases.mainDispatcher
-import io.ktor.client.HttpClient
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.get
-import io.ktor.http.takeFrom
-import io.ktor.utils.io.*
-import io.ktor.utils.io.core.internal.*
-import koin.httpEngine
-import kotlinx.atomicfu.AtomicBoolean
-import kotlinx.atomicfu.AtomicRef
-import kotlinx.atomicfu.atomic
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.internal.AtomicOp
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
-import org.koin.core.KoinComponent
-import org.koin.core.inject
-import kotlin.coroutines.coroutineContext
-import kotlin.native.concurrent.SharedImmutable
-import kotlin.native.concurrent.ThreadLocal
 
 class RemoteFinance(private val client: HttpClient) : RemoteFinanceInterface {
 
