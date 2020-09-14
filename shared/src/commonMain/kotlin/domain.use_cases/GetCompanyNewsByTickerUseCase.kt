bdfa8e48123d1f6ae181b49b2da083232e5a583f
Max <maximilian.fehrentz@envidual.com>
2020-09-14 10:52:34 +0200

    added Throws in GetCompanyNewsByTickerUseCase for iOS to know the Exception that might occur

--- shared/src/commonMain/kotlin/domain.use_cases/GetCompanyNewsByTickerUseCase.kt
+++ shared/src/commonMain/kotlin/domain.use_cases/GetCompanyNewsByTickerUseCase.kt
@@ -21,7 +21,7 @@ class GetCompanyNewsByTickerUseCase(
     private val remoteFinance: RemoteFinanceInterface
 ) {
 
-
+    @Throws(Exception::class, CompanyNewsNotFoundException::class)
     suspend fun invoke(ticker: String): List<CompanyNews> {
 //        try {
 //            remoteFinance.freeze()
